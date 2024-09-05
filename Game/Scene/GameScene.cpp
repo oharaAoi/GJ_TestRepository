8a40ea2ffbad8031123ee34a08be9d4c48a87b56
oharaAoi <k023g0108@g.neec.ac.jp>
2024-09-05 12:54:14 +0900

    GameSceneの作成をしました。

---
+++ Game/Scene/GameScene.cpp
@@ -0,0 +1,22 @@
+#include "GameScene.h"
+
+void GameScene::initialize() {
+}
+
+void GameScene::begin() {
+}
+
+void GameScene::update() {
+}
+
+void GameScene::begin_rendering() {
+}
+
+void GameScene::late_update() {
+}
+
+void GameScene::draw() const {
+}
+
+void GameScene::debug_update() {
+}
