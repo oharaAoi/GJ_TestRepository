846e5a9b1d6c9c9769004746d34094a7e7710805
oharaAoi <k023g0108@g.neec.ac.jp>
2024-09-05 15:23:45 +0900

    ・playerの描画処理を追加 ・gameSceneにコードを書くように変更

--- TestCode/SceneDemo.cpp
+++ TestCode/SceneDemo.cpp
@@ -15,15 +15,6 @@ void SceneDemo::load() {
 }
 
 void SceneDemo::initialize() {
-	planet_ = std::make_unique<Planet>();
-
-	camera3D_ = std::make_unique<Camera3D>();
-	camera3D_->initialize();
-	camera3D_->set_transform({
-		CVector3::BASIS,
-		Quaternion::EulerDegree(45, 0, 0),
-		{ 0, 10, -10 }
-	});
 }
 
 void SceneDemo::finalize() {
@@ -36,22 +27,15 @@ void SceneDemo::begin() {
 }
 
 void SceneDemo::update() {
-	camera3D_->update_matrix();
-	camera3D_->update();
 }
 
 void SceneDemo::begin_rendering() {
-	planet_->begin_rendering(*camera3D_);
 }
 
 void SceneDemo::late_update() {
 }
 
 void SceneDemo::draw() const {
-	RenderPathManager::BeginFrame();
-	DirectXCore::ShowGrid(*camera3D_);
-	planet_->draw();
-	RenderPathManager::Next();
 }
 
 #ifdef _DEBUG
@@ -68,10 +52,6 @@ void SceneDemo::debug_update() {
 	if (ImGui::Button("PopScene")) {
 		SceneManager::PopScene();
 	}
-	ImGui::End();
-
-	ImGui::Begin("Camera3D");
-	camera3D_->debug_gui();
 	ImGui::End();
 }
 #endif // _DEBUG
