37306e756bfd7697028b3f4c76711432799237c1
oharaAoi <k023g0108@g.neec.ac.jp>
2024-09-06 16:18:49 +0900

    ManagetクラスとTimedCallクラスを追加

---
+++ Game/WorldObject/MeteoriteManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Game/WorldObject/Meteorite.h"
+
+class MeteoriteManager {
+public:
+
+	MeteoriteManager();
+	~MeteoriteManager();
+
+	/// <summary>
+	/// 初期化関数
+	/// </summary>
+	void Init();
+
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	void Update();
+
+private:
+
+
+
+
+};
+
