cdd9da9a6479c617f1c5bb7236ebba8272617b49
oharaAoi <k023g0108@g.neec.ac.jp>
2024-09-13 14:07:44 +0900

    満足度のループが続くバグを修正

--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -345,6 +345,8 @@ void GameScene::update() {
 
 	enemyManager_->Update(player_->get_transform().get_translate());
 
+	effectManager_->Update();
+
 	posteffectManager->update();
 
 	// -------------------------------------------------
